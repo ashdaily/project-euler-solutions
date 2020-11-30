da8a0d850293c69f2dcfd668cb811146a76912dd
ashdaily <ashishsinghbardhan@gmail.com>
2020-11-30 22:36:12 +0900

    problem001 in python and js

--- problem001.js
+++ problem001.js
@@ -1,25 +1,23 @@
-// https://projecteuler.net/problem=1
-
 /*
+https://projecteuler.net/problem=1
 If we list all the natural numbers below 10 that are multiples
 of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
 Find the sum of all the multiples of 3 or 5 below 1000.
 */
 
-// multiples of 3 -> 3, 6, 9, 12, 15 ....
-// multiples of 5 ->  5,  10, 15, 20 ....
 
-function findSumOfMultiplesOfNumber(number){
-    var sumOfAllMutiples = 0;
-    for(var i=0; i<1000; i+=number){
-        if(i%number === 0){
+const findSolution = (number1, number2)=> {
+    let sumOfAllMutiples = 0;
+    let i =  0;
+    while (i < 1000){
+        if(i % number1 === 0 || i % number2 === 0){
             sumOfAllMutiples += i;
         }
+        i += 1;
     }
     return sumOfAllMutiples;
 }
 
-var sumOf3 = findSumOfMultiplesOfNumber(3)
-var sumOf5 = findSumOfMultiplesOfNumber(5)
-
-console.log(sumOf3+sumOf5)
+const number1 = 3;
+const number2 = 5;
+findSolution(number1, number2)
